f50b54478128abde3761ff51cdeb50ba9a44f586
Ritvik kant <ritvikkantip27@gmail.com>
2026-03-20 15:23:43 +0530

    Add CI/Cd and corrected deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,51 +1,36 @@
 #!/bin/bash
 set -e
-
 echo "Starting deployment..."
-
-APP_DIR=~/app
-FRONTEND_DIR=$APP_DIR/src/apps/frontend         # package.json is at repo root
+APP_DIR="$(cd "$(dirname "$0")" && pwd)"
 BACKEND_DIR=$APP_DIR/src/apps/backend
 NGINX_DIR=/var/www/html
+START=$(date +%s)
 
-START=$(date +%s) #time of process
-
-#pull altest code
 echo "Pulling latest code..."
 cd $APP_DIR
 git pull origin main
 
-#frontend building
 echo "Building frontend..."
-cd $FRONTEND_DIR
+cd $APP_DIR
 npm ci
 npm run build
 
 echo "Deploying frontend..."
 sudo rm -rf $NGINX_DIR/*
-sudo cp -r build/* $NGINX_DIR/ #update and rebuild nginx server 
+sudo cp -r $APP_DIR/dist/public/* $NGINX_DIR/
 
-#Backend
 echo "Setting up backend..."
 cd $BACKEND_DIR
-
 if [ ! -d "venv" ]; then
-  python3 -m venv venv
+    python3 -m venv venv
 fi
-
 source venv/bin/activate
 pip install -r requirements.txt
 
-nohup venv/bin/gunicorn --bind unix:/tmp/app.sock server:app > app.log 2>&1 &
-
-#Restarting abckend
 echo "Restarting backend..."
+pkill -f gunicorn || true
+nohup venv/bin/gunicorn --bind unix:/tmp/app.sock server:app > gunicorn.log 2>&1 &
 
-pkill -f gunicorn || true # checking if there is no availbe running process
-
-nohup venv/bin/gunicorn --bind unix:/tmp/app.sock app:app > gunicorn.log 2>&1 &
-
-# REload nginx
 echo "Reloading Nginx..."
 sudo nginx -t
 sudo systemctl reload nginx
